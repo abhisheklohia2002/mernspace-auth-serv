49c86bebf7d277fae24af42640acb198f85a89c0
Abhishek lohia <abhisheklohia@tripxl.com>
2026-02-02 23:50:03 +0530

    add setup of Express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
-import express from "express";
+import express, { type NextFunction ,type Response,type Request} from "express";
+import logger from "./config/logger.js";
+import type { HttpError } from "http-errors";
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Welcome to the Auth Service");
+    res.send("Welcome to the Auth Service");
 });
+// Error handling middleware
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err:HttpError,req:Request,res:Response,next:NextFunction)=>{
+    logger.error(`Error occurred: ${err.message}`);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+       error:[
+        {
+            type:err.name,
+            message:err.message,
+            path:"",
+            location:"",
+        }
+       ] 
+    })
+})
 export default app;
