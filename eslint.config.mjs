fbf0ac112e73f0ff722a7b74b8405dd4fbcef22b
Abhishek lohia <abhisheklohia@tripxl.com>
2026-02-02 22:09:58 +0530

    add husky

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+
+    rules: {},
+  },
+  {
+    ignores: ["dist/**", "node_modules","eslint.config.mjs" ],
+  },
+);
