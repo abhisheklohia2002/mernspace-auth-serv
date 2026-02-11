a4731cbae305d17db63398125efaa5d35c70ed60
Abhishek lohia <abhisheklohia@tripxl.com>
2026-02-11 15:42:01 +0530

    add one middleware into express app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-
 import "reflect-metadata";
-import express, { type NextFunction ,type Response,type Request} from "express";
+import express, {
+  type Response,
+  type Request,
+} from "express";
 import logger from "./config/logger.js";
 import type { HttpError } from "http-errors";
 import router from "./routes/auth.js";
@@ -16,39 +18,46 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin:[Config.FRONTEND_ADMIN_DOMAIN],
-    credentials:true
-}))
-app.use(cookieParser())
-app.use("/api/auth",router)
-app.use("/api/tenant",tenantRouter)
-app.use("/api/user",userRouter)
+app.use(
+  cors({
+    origin: [Config.FRONTEND_ADMIN_DOMAIN],
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+app.use("/api/auth", router);
+app.use("/api/tenant", tenantRouter);
+app.use("/api/user", userRouter);
 
-
-app.use(express.static(path.join(__dirname, "../public"), { dotfiles: "allow" }));
+app.use(
+  express.static(path.join(__dirname, "../public"), { dotfiles: "allow" }),
+);
 app.get("/.well-known/jwks.json", (req, res) => {
   res.sendFile("jwks.json", { root: "public/.well-known" });
 });
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to the Auth Service</h1>");
+  res.send("<h1>Welcome to the Auth Service</h1>");
 });
 
 // Error handling middleware
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err:HttpError,req:Request,res:Response,next:NextFunction)=>{
-    logger.error(`Error occurred: ${err.message}`);
-    const statusCode = err.statusCode || 500;
-    res.status(statusCode).json({
-       error:[
-        {
-            type:err.name,
-            message:err.message,
-            path:"",
-            location:"",
-        }
-       ] 
-    })
-})
+ 
+
+app.use((err: HttpError, req: Request, res: Response) => {
+  logger.error(`Error occurred: ${err.message}`);
+  const statusCode = err.statusCode || 500;
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  res.status(statusCode).json({
+    error: [
+      {
+        type: err.name,
+        message: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
+});
 
 export default app;
